32f710d8dce6020e0110f1f7b6b7376f12926622
habib <40292697+habibadeku@users.noreply.github.com>
2021-01-23 23:38:50 +0100

    updates to attempt fix to run test in git actions

--- e2e/protractor-ci.conf.js
+++ e2e/protractor-ci.conf.js
@@ -3,9 +3,8 @@
     config.capabilities = {
         browserName: 'chrome',
         chromeOptions: {
-            args: ['--headless', '--no-sandbox','--disable-gpu']
+            args: ['--headless', '--no-sandbox']
         },
-        chromeDriver:"./node_modules/webdriver-manager/selenium/chromedriver_84.0.4147.30"
     };
 
     exports.config = config;
