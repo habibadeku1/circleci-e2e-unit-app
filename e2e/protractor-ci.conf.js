f1182640ac4c564c716cc3adc15d70c82ac7c2e1
habib <40292697+habibadeku@users.noreply.github.com>
2021-01-25 11:24:04 +0100

    add some reporting and storing to circleci artifacts

--- e2e/protractor-ci.conf.js
+++ e2e/protractor-ci.conf.js
@@ -1,10 +1,21 @@
     const config = require('./protractor.conf').config;
 
+    const HtmlReporter = require('protractor-html-screenshot-reporter');
+    const reporter = new HtmlReporter({
+       baseDirectory: './protractor-result', // a location to store screen shots.
+       docTitle: 'Protractor Demo Reporter',
+       docName: 'protractor-demo-tests-report.html'
+    });
+
     config.capabilities = {
         browserName: 'chrome',
         chromeOptions: {
             args: ['--headless', '--no-sandbox']
         },
     };
+
+    config.onPrepare = function() {
+      jasmine.getEnv().addReporter(reporter);
+    }
 
     exports.config = config;
